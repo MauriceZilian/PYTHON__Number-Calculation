ce2180afe056bdc2a88586782f75cbf63032e1bb
Maurice Zilian <mauricezilian1992@web.de>
2025-06-16 15:58:11 +0200

    zahlenberechnung_structured.py
    zahlenberechnung_structured.py

--- structured/zahlenberechnung_structured.py
+++ structured/zahlenberechnung_structured.py
@@ -1,45 +1,41 @@
-def main():
-    # Initialisierung
-    summe = 0.0
-    anzahl = 0
-    minimum = None
-    maximum = None
+# Initialisierung
+summe = 0.0
+anzahl = 0
+minimum = None
+maximum = None
 
-    # Eingabeschleife
-    while True:
-        eingabe = input("Bitte eine Zahl eingeben (oder 'fertig' zum Beenden): ")
+# Eingabeschleife
+while True:
+    eingabe = input("Bitte eine Zahl eingeben (oder 'fertig' zum Beenden): ")
 
-        if eingabe.lower() == 'fertig':
-            break
+    if eingabe.lower() == 'fertig':
+        break
 
-        try:
-            zahl = float(eingabe)
-            summe += zahl
-            anzahl += 1
+    try:
+        zahl = float(eingabe)
+        summe += zahl
+        anzahl += 1
 
-            if minimum is None or zahl < minimum:
-                minimum = zahl
+        if minimum is None or zahl < minimum:
+            minimum = zahl
 
-            if maximum is None or zahl > maximum:
-                maximum = zahl
+        if maximum is None or zahl > maximum:
+            maximum = zahl
 
-        except ValueError:
-            print("Ungültige Eingabe. Bitte eine gültige Zahl eingeben.")
+    except ValueError:
+        print("Ungültige Eingabe. Bitte eine gültige Zahl eingeben.")
+        
+# Ausgabe
+print("\nErgebnisse:")
+print("Anzahl:", anzahl)
+print("Summe:", summe)
 
-    # Ausgabe
-    print("\nErgebnisse:")
-    print("Anzahl:", anzahl)
-    print("Summe:", summe)
+if anzahl > 0:
+    durchschnitt = summe / anzahl
+    print("Durchschnitt:", durchschnitt)
+else:
+    print("Kein Durchschnitt berechenbar.")
 
-    if anzahl > 0:
-        durchschnitt = summe / anzahl
-        print("Durchschnitt:", durchschnitt)
-    else:
-        print("Kein Durchschnitt berechenbar.")
+print("Minimum:", minimum)
+print("Maximum:", maximum)
 
-    print("Minimum:", minimum)
-    print("Maximum:", maximum)
-
-
-if __name__ == "__main__":
-    main()
